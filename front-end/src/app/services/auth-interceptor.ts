3dbfa64f11eff1c2bc43b5ad8ba1f58b4ed1c26f
Zaina <32458609+ZainaMALEK@users.noreply.github.com>
2023-05-06 13:53:04 +0200

    choix de la categorie necessaire pour ajouter un produit

--- front-end/src/app/services/auth-interceptor.ts
+++ front-end/src/app/services/auth-interceptor.ts
@@ -7,12 +7,14 @@ export class AuthInterceptor implements HttpInterceptor {
     const authToken = localStorage.getItem('jwt'); // Récupérer le jeton d'authentification depuis le stockage local
     if (authToken) {
       // Si le jeton existe, l'ajouter à l'en-tête de la requête
+
       const authReq = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${authToken}`)
       });
       return next.handle(authReq);
     } else {
       // Si le jeton n'existe pas, envoyer la requête telle quelle
+
       return next.handle(req);
     }
   }
